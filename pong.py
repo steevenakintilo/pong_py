0298b091240ec4066c5bce07fe94b6bedf847667
steevenakintilo <72024403+steevenakintilo@users.noreply.github.com>
2021-08-15 14:15:59 +0200

    Uptade the ending and display.

--- pong.py
+++ pong.py
@@ -57,6 +57,11 @@ class Player2(pygame.sprite.Sprite):
         self.surf.fill((255, 0, 0))
         self.rect = self.surf.get_rect()
 
+class Cup(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cup, self).__init__()
+        self.surf = pygame.image.load("cup.png").convert_alpha()
+        self.rect = self.surf.get_rect()
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
@@ -108,8 +113,8 @@ def menu_loop():
 
     pygame.init()
     pygame.mixer.init()
-    GAME_FONT = pygame.freetype.Font("font.ttf", 44)
-    GAME_FONT2 = pygame.freetype.Font("font.ttf", 74)
+    GAME_FONT = pygame.freetype.Font("arcade.ttf", 44)
+    GAME_FONT2 = pygame.freetype.Font("arcade.ttf", 74)
     #pygame.mixer.Channel(0).play(pygame.mixer.Sound('music.ogg'),999)
     #pygame.mixer.Channel(1).play(pygame.mixer.Sound('boom.ogg'))
 
@@ -141,8 +146,7 @@ def menu_loop():
         GAME_FONT.render_to(screen, (120, 230), "PRESS A TO PLAY AGAINST IA", (255, 255, 255))
         GAME_FONT.render_to(screen, (120, 330), "PRESS B TO PLAY VS", (255, 255, 255))
         GAME_FONT.render_to(screen, (120, 430), "PRESS C TO SEE IA VS IA", (255, 255, 255))
-        #GAME_FONT.render_to(screen, (0, 20), "HIGHSCORE: " + str(high), (255, 255, 255))
-
+        
         pygame.display.flip()
         clock.tick(30)
 
@@ -153,18 +157,15 @@ def end_loop(MOD):
 
     pygame.init()
     pygame.mixer.init()
-    GAME_FONT = pygame.freetype.Font("font.ttf", 44)
-    #pygame.mixer.Channel(0).play(pygame.mixer.Sound('music.ogg'),999)
-    #pygame.mixer.Channel(1).play(pygame.mixer.Sound('boom.ogg'))
-
+    GAME_FONT = pygame.freetype.Font("arcade.ttf", 44)
+  
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     X = 100
     Y = SCREEN_HEIGHT/2
 
     clock = pygame.time.Clock()
     running = True
-
-    # Main loop
+    cup = Cup()
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
@@ -177,27 +178,25 @@ def end_loop(MOD):
             menu_loop()
         
         screen.fill((0, 0, 0))
-        
+        screen.blit(cup.surf, (300,150))
         if MOD == 1:
-            GAME_FONT.render_to(screen, (200, 30), "PLAYER1 YOU WON", (255, 255, 255))
+            GAME_FONT.render_to(screen, (230, 30), "PLAYER1 YOU WON", (255, 255, 255))
         if MOD == 2:
-            GAME_FONT.render_to(screen, (200, 30), "PLAYER2 YOU WON", (255, 255, 255))
+            GAME_FONT.render_to(screen, (230, 30), "PLAYER2 YOU WON", (255, 255, 255))
         if MOD == 3:
-            GAME_FONT.render_to(screen, (200, 30), "IA YOU WON", (255, 255, 255))
+            GAME_FONT.render_to(screen, (280, 30), "IA YOU WON", (255, 255, 255))
         if MOD == 4:
-            GAME_FONT.render_to(screen, (200, 30), "IA BLUE YOU WON", (255, 255, 255))
+            GAME_FONT.render_to(screen, (235, 30), "IA BLUE YOU WON", (255, 255, 255))
         if MOD == 5:
-            GAME_FONT.render_to(screen, (200, 30), "IA RED YOU WON", (255, 255, 255))
-        
+            GAME_FONT.render_to(screen, (235, 30), "IA RED YOU WON", (255, 255, 255))
         GAME_FONT.render_to(screen, (120, 530), "PRESS SPACE TO GO TO THE MENU", (255, 255, 255))
-        #GAME_FONT.render_to(screen, (0, 20), "HIGHSCORE: " + str(high), (255, 255, 255))
-
+      
         pygame.display.flip()
         clock.tick(30)
 
 def main_game_vs():
     pygame.init()
-    GAME_FONT = pygame.freetype.Font("font.ttf", 30)
+    GAME_FONT = pygame.freetype.Font("arcade.ttf", 60)
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     P1X = 10
@@ -219,17 +218,12 @@ def main_game_vs():
     line = Line()
     running = True
     SPEED = 0
-    # Main loop
     while running:
         SPEED = SPEED + 0.02
-        # for loop through the event queue
         for event in pygame.event.get():
-            # Check for KEYDOWN event
             if event.type == KEYDOWN:
-                # If the Esc key is pressed, then exit the main loop
                 if event.key == K_ESCAPE:
                     running = False
-            # Check for QUIT event. If QUIT, then set running to false.
             elif event.type == QUIT:
                 running = False
         pressed_keys = pygame.key.get_pressed()
@@ -242,8 +236,6 @@ def main_game_vs():
             P1Y = P1Y - 10
         if pressed_keys[ord('s')] and P1Y < 495:
             P1Y = P1Y + 10
-        
-        # BALL MOVEMENT
         if direction == 1:
             BALLX = BALLX - 2.5 - SPEED
         if direction == 2:
@@ -308,10 +300,8 @@ def main_game_vs():
             pygame.mixer.Channel(0).play(pygame.mixer.Sound('score.ogg'))
         if POINT1 == 10:
             end_loop(1)
-        if POINT2 == 10:
+        if POINT2 == 1:
             end_loop(2)
-        #else:
-        #    BALLX = BALLX + 5
         screen.fill((0, 0, 0))
         screen.blit(player1.surf, (P1X,P1Y))
         screen.blit(player2.surf, (P2X,P2Y))
@@ -326,13 +316,12 @@ def main_game_vs():
         
         GAME_FONT.render_to(screen, (170, 30), str(POINT1), (255, 255, 255))          
         GAME_FONT.render_to(screen, (580, 30), str(POINT2), (255, 255, 255))
-        #print(BALLX,P1Y,P2Y)
         pygame.display.flip()
         clock.tick(30)
     
 def main_game_ia():
     pygame.init()
-    GAME_FONT = pygame.freetype.Font("font.ttf", 30)
+    GAME_FONT = pygame.freetype.Font("arcade.ttf", 60)
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     P1X = 10
@@ -356,40 +345,25 @@ def main_game_ia():
     rdm_level = randint(0,200)
     running = True
     SPEED = 0
-    # Main loop
     while running:
         SPEED = SPEED + 0.02
-        # for loop through the event queue
         for event in pygame.event.get():
-            # Check for KEYDOWN event
             if event.type == KEYDOWN:
-                # If the Esc key is pressed, then exit the main loop
                 if event.key == K_ESCAPE:
                     running = False
-            # Check for QUIT event. If QUIT, then set running to false.
             elif event.type == QUIT:
                 running = False
         pressed_keys = pygame.key.get_pressed()
         
-        #if pressed_keys[K_UP] and P2Y > 5:
-        #    P2Y = P2Y - 10
-        #if pressed_keys[K_DOWN] and P2Y < 495:
-        #    P2Y = P2Y + 10
         if pressed_keys[ord('z')] and P1Y > 5:
             P1Y = P1Y - 10
         if pressed_keys[ord('s')] and P1Y < 495:
             P1Y = P1Y + 10
-        #if X == 1 and P2Y > 10 and P2Y < 490:
-        #    P2Y = BALLY - 20
-        #elif X == 2 and P2Y > 10 and P2Y < 490:
-        #    P2Y = BALLY + 20
         P2Y = BALLY - rdm_level
         if P2Y >= 495:
             P2Y = 490
         if P2Y <= 5:
             P2Y = 10
-        #print(P2Y,BALLY,X)
-        # BALL MOVEMENT
         if direction == 1:
             BALLX = BALLX - 2.5 - SPEED
         if direction == 2:
@@ -460,8 +434,6 @@ def main_game_ia():
             end_loop(1)
         if POINT2 == 10:
             end_loop(3)
-        #else:
-        #    BALLX = BALLX + 5
         screen.fill((0, 0, 0))
         screen.blit(player1.surf, (P1X,P1Y))
         screen.blit(player2.surf, (P2X,P2Y))
@@ -472,16 +444,13 @@ def main_game_ia():
         screen.blit(line.surf, (385,345))
         screen.blit(line.surf, (385,460))
         screen.blit(line.surf, (385,575))
-        
-        
         GAME_FONT.render_to(screen, (170, 30), str(POINT1), (255, 255, 255))          
         GAME_FONT.render_to(screen, (580, 30), str(POINT2), (255, 255, 255))
-        #print(BALLX,P1Y,P2Y)
         pygame.display.flip()
         clock.tick(30)
 def main_game_ia_vs_ia():
     pygame.init()
-    GAME_FONT = pygame.freetype.Font("font.ttf", 30)
+    GAME_FONT = pygame.freetype.Font("arcade.ttf", 60)
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     P1X = 10
@@ -507,29 +476,16 @@ def main_game_ia_vs_ia():
     SPEED = 0
     rdm_level = randint(0,200)
     rdm_level2 = randint(0,200)
-    # Main loop
     while running:
         SPEED = SPEED + 0.02
-        # for loop through the event queue
         for event in pygame.event.get():
-            # Check for KEYDOWN event
             if event.type == KEYDOWN:
-                # If the Esc key is pressed, then exit the main loop
                 if event.key == K_ESCAPE:
                     running = False
-            # Check for QUIT event. If QUIT, then set running to false.
             elif event.type == QUIT:
                 running = False
         pressed_keys = pygame.key.get_pressed()
         
-        #if pressed_keys[K_UP] and P2Y > 5:
-        #    P2Y = P2Y - 10
-        #if pressed_keys[K_DOWN] and P2Y < 495:
-        #    P2Y = P2Y + 10
-        #if X == 1 and P2Y > 10 and P2Y < 490:
-        #    P2Y = BALLY - 20
-        #elif X == 2 and P2Y > 10 and P2Y < 490:
-        #    P2Y = BALLY + 20
         P2Y = BALLY - rdm_level
         P1Y = BALLY - rdm_level2
         if P2Y >= 495:
@@ -540,8 +496,6 @@ def main_game_ia_vs_ia():
             P1Y = 490
         if P1Y <= 5:
             P1Y = 10
-        #print(P2Y,BALLY,X)
-        # BALL MOVEMENT
         if direction == 1:
             BALLX = BALLX - 2.5 - SPEED
             direction = 3
@@ -616,8 +570,6 @@ def main_game_ia_vs_ia():
             end_loop(4)
         if POINT2 == 10:
             end_loop(5)
-        #else:
-        #    BALLX = BALLX + 5
         screen.fill((0, 0, 0))
         screen.blit(player1.surf, (P1X,P1Y))
         screen.blit(player2.surf, (P2X,P2Y))
